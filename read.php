b030b00ada0620d2a20add4e1d62c2e53b70af63
esplanadecloud <122422793+esplanadecloud@users.noreply.github.com>
2023-01-11 12:02:14 +0800

    Add files via upload

---
+++ read.php
@@ -0,0 +1,38 @@
+<result>
+<row>
+<eventtitle>In Between</eventtitle>
+<eventimage>https://www.esplanade.com/-/media/Esplanade/Images/Whats-On/festival-and-series/2023/all-things-new/in-between-01.jpg</eventimage>
+<eventartist>aeriqah</eventartist>
+<eventcountry>Singapore</eventcountry>
+<eventstart>19:00</eventstart>
+<eventend>19:40</eventend>
+<displaytiming>7pm</displaytiming>
+<eventfestival>All Things New 2023</eventfestival>
+<eventvenue>Esplanade Concourse</eventvenue>
+<synopsis>Laugh, cry and sit in your feelings with the emotive music of singer-songwriter aeriqah. 
+</synopsis>
+<ticketed>free-performance</ticketed>
+<titleotherlang></titleotherlang>
+<language></language>
+<advisory>Mask-wearing is optional. However, audiences are encouraged to continue to
+exercise responsibility and caution, such as wearing masks when in crowded
+places.  No tickets will be issued. Seats are available on a first-come,
+first-served basis. 
+
+</advisory>
+</row><row>
+<eventtitle>Wax and Wane</eventtitle>
+<eventimage>https://www.esplanade.com/-/media/Esplanade/Images/Whats-On/festival-and-series/2022/visual-arts/wax-and-wane-01.jpg</eventimage>
+<eventartist>RSCLS</eventartist>
+<eventcountry>Singapore</eventcountry>
+<eventstart></eventstart>
+<eventend>23:59</eventend>
+<displaytiming>All day</displaytiming>
+<eventfestival>Visual Arts</eventfestival>
+<eventvenue>Singtel Waterfront Theatre</eventvenue>
+<synopsis>Commissioned in conjunction with the opening of the Singtel Waterfront Theatre at Esplanade and the centre’s 20th anniversary celebrations, the sculptural mural Wax and Wane was created collaboratively by Antz, DEM, TraseOne, SPAZ and ZERO from the urban art collective RSCLS.</synopsis>
+<ticketed></ticketed>
+<titleotherlang></titleotherlang>
+<language></language>
+<advisory></advisory>
+</row></result>
